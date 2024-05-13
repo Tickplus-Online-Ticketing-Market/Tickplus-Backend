700ba0013785d562f2fdce324d0e17486eaa6a31
rasinduanuththara <rasinduanuththara@gmail.com>
2024-05-13 17:06:42 +0530

    routing setup

---
+++ routes/SponsershipManagementRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const exampleController = require("../controllers/_ExampleController/_ExampleController");
+
+router.post("/", exampleController.createExample); // create
+router.get("/:id", exampleController.retrieveExample); // retrieve
+router.put("/:id", exampleController.updateExample); // update
+router.delete("/:id", exampleController.deleteExample); // delete
+router.get("/", exampleController.retrieveAllExamples); // retrieve all
+
+module.exports = router;
