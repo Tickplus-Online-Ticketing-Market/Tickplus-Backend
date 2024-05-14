5c5ec4a565f54ab4c67a8a31fac20810833bdf93
Chinthana Weligamage <chinthanaweligamage1@gmail.com>
2024-05-14 12:04:48 +0530

    Error Handling

--- controllers/TicketPurchase/TicketsController.js
+++ controllers/TicketPurchase/TicketsController.js
@@ -2,33 +2,41 @@ const Tickets = require("../../models/Tickets");
 
 // Fetch all records
 const fetchAllTickets = async (req, res) => {
-    const allTickets = await Tickets.find();
-    res.json({ Tickets: allTickets });
+  const allTickets = await Tickets.find();
+  res.json({ Tickets: allTickets });
 };
 
 // Fetch one record
 const fetchOneTicket = async (req, res) => {
+  try {
     const ticketId = req.params.id;
     const ticket = await Tickets.findById(ticketId);
     if (!ticket) {
-        return res.json({ error: "Ticket not found" });
+      return res.json({ error: "Ticket not found" });
     }
     res.json({ Tickets: ticket });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Create a record
 const createTicket = async (req, res) => {
+  try {
     const { eventId, eventName, unitPrice } = req.body;
     const ticket = await Tickets.create({
-        eventId,
-        eventName,
-        unitPrice
+      eventId,
+      eventName,
+      unitPrice,
     });
     res.json({ Tickets: ticket });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
-    fetchAllTickets,
-    fetchOneTicket,
-    createTicket,
+  fetchAllTickets,
+  fetchOneTicket,
+  createTicket,
 };
