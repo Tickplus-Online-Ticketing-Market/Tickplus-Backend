0d77845944ca8b648db43b04e32de8b47eecef84
Chinthana Weligamage <chinthanaweligamage1@gmail.com>
2024-05-15 22:27:17 +0530

    Revert "Digital customization backend dev completed"
    This reverts commit 48d74f09d4f4ca6fff46a21533a5634855bd9a05.

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const ConnectToDB = require("./config/ConnectToDB");
 
 // Import routes
 const exampleRoutes = require("./routes/_ExapmleRoutes");
-const DigitalCustomizationRoutes = require("./routes/DigitalCustomization");
 
 // Create an Express app
 const app = express();
@@ -18,7 +17,7 @@ const app = express();
 // Configure express app
 app.use(express.json());
 app.use(cors());
-app.use(express.static("uploads/images"));
+
 // Connect to DB
 ConnectToDB();
 
@@ -29,7 +28,6 @@ app.get("/", (req, res) => {
 
 // Component Routing
 app.use("/example", exampleRoutes);
-app.use("/digital-customization",DigitalCustomizationRoutes);
 
 // Start the server
 app.listen(process.env.PORT);
