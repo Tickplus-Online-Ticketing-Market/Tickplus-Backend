e9553797a26d476bbe3b07626d4bbaf3d4bfb16b
Chinthana Weligamage <chinthanaweligamage1@gmail.com>
2024-05-15 22:28:26 +0530

    Revert "Digital customization backend dev completed" (#2)
    This reverts commit 48d74f09d4f4ca6fff46a21533a5634855bd9a05.

--- routes/_ExapmleRoutes.js
+++ routes/_ExapmleRoutes.js
@@ -7,5 +7,5 @@ router.get("/:id", exampleController.retrieveExample); // retrieve
 router.put("/:id", exampleController.updateExample); // update
 router.delete("/:id", exampleController.deleteExample); // delete
 router.get("/", exampleController.retrieveAllExamples); // retrieve all
-// test
+
 module.exports = router;
