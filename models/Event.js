789429b5907c6a656c8647cbacc27db25777d9bc
Chinthana Weligamage <chinthanaweligamage1@gmail.com>
2024-05-16 09:31:18 +0530

    Event Launching debug

--- models/Event.js
+++ models/Event.js
@@ -1,11 +1,9 @@
-//sets up a schema for event
-
 const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema; //Destructures the Schema class from Mongoose
+const Schema = mongoose.Schema; // Destructures the Schema class from Mongoose
 
 const eventSchema = new Schema({
-  //Defines a new schema for events with the following fields
+  // Defines a new schema for events with the following fields
   image: {
     type: String,
     required: true,
@@ -38,67 +36,12 @@ const eventSchema = new Schema({
     type: Number,
     required: true,
   },
-  // downloadCount:{
+  // Uncomment these if needed
+  // downloadCount: {
   //   type: Number,
-
   // },
-
-  // requestCount:{
+  // requestCount: {
   //   type: Number,
-
-  // },
-});
-
-module.exports = mongoose.model("Event", eventSchema);
-
-//sets up a schema for event
-
-const mongoose = require("mongoose");
-
-const Schema = mongoose.Schema; //Destructures the Schema class from Mongoose
-
-const eventSchema = new Schema({
-  //Defines a new schema for events with the following fields
-  image: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  venue: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-  },
-  artist: {
-    type: String,
-    required: true,
-  },
-  about: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  // downloadCount:{
-  //   type: Number,
-
-  // },
-
-  // requestCount:{
-  //   type: Number,
-
   // },
 });
 
