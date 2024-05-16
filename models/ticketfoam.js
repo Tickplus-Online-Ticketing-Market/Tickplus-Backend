ca62b4bfd64806bb762c2017456eb0c46bd6e936
Devram-ts <devramsaparamadu@gmail.com>
2024-05-16 10:02:56 +0530

    final 100%

--- models/ticketfoam.js
+++ models/ticketfoam.js
@@ -1,30 +1,38 @@
 const mongoose = require("mongoose");
 
-// Define the function to generate a random ticket ID
-const generateTicketId = () => {
-  const randomDigits = Math.floor(Math.random() * 10000000); // Generate random 4-digit number
-  return `TIK${randomDigits}`;
-};
+let Ticketfoam; // Declare the model variable
 
-// Define the ticket schema
-const ticketSchema = new mongoose.Schema({
-  eventname: String,
-  date: String,
-  venue: String,
-  time: String,
-  ticketQuantity: String,
-  ticketPrice: String,
-  ticketStatus: String,
-  ticketMode: String,
-  imageUrl: String, // Add imageUrl field
-  ticketId: {
-    type: String,
-    default: generateTicketId, // Use the generateTicketId function as default value
-  },
-  created_at: { type: String, default: () => new Date().toISOString() },
-});
+// Check if the model exists
+if (mongoose.models && mongoose.models.Ticketfoam) {
+  // If the model exists, re-use it
+  Ticketfoam = mongoose.model("Ticketfoam");
+} else {
+  // Define the function to generate a random ticket ID
+  const generateTicketId = () => {
+    const randomDigits = Math.floor(Math.random() * 10000000); // Generate random 4-digit number
+    return `TIK${randomDigits}`;
+  };
 
-// Create the Ticketfoam model
-const Ticketfoam = mongoose.model("Ticketfoam", ticketSchema);
+  // Define the ticket schema
+  const ticketSchema = new mongoose.Schema({
+    eventname: String,
+    date: String,
+    venue: String,
+    time: String,
+    ticketQuantity: String,
+    ticketPrice: String,
+    ticketStatus: String,
+    ticketMode: String,
+    imageUrl: String, // Add imageUrl field
+    ticketId: {
+      type: String,
+      default: generateTicketId, // Use the generateTicketId function as default value
+    },
+    created_at: { type: String, default: () => new Date().toISOString() },
+  });
+
+  // Create the Ticketfoam model
+  Ticketfoam = mongoose.model("Ticketfoam", ticketSchema);
+}
 
 module.exports = Ticketfoam;
